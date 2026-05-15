71826e1d64e2051d7c558656596254ec77a469ab
Alex Coxe <rot4te@gmail.com>
2026-05-15 19:32:30 -0400

    fixed broken extension path

--- src/RadiativeViewFactor.jl
+++ src/RadiativeViewFactor.jl
@@ -15,8 +15,6 @@ include("ViewFactorKernel.jl")
 include("GPUKernels.jl")
 include("Assembly.jl")
 include("GPUAssembly.jl")
-include("RadiativeViewFactorCUDAExt.jl")
-include("RadiativeViewFactorMetalExt.jl")
 
 using .MeshIO:    load_mesh, MeshData
 using .Assembly:  compute_view_factors, aggregate_by_group,
